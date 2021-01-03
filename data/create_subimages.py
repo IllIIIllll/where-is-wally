d3673563c2b6394c515b0135d9f391c8ba0eb23d
JISUNG PARK <llllllllll@kakao.com>
2021-01-03 18:54:19 +0900

    feat: Find the bounding box

--- data/create_subimages.py
+++ data/create_subimages.py
@@ -2,6 +2,8 @@
 # <llllllllll@kakao.com>
 # MIT License
 
+import numpy as np
+
 def extract_224_sub_image(img, box):
     wstart, wend, hstart, hend = box
     i = img[wend-224:wstart+224, hend-224:hstart+224]
@@ -9,3 +11,12 @@ def extract_224_sub_image(img, box):
         return img[wstart:wstart+225, hend-224:hstart+224]
     else:
         return i
+
+def find_box(trg):
+    h = np.max(np.unique(trg.sum(axis=1)))
+    hstart = np.argmax(trg.sum(axis=0))
+    hend = int(hstart+h)
+    w = np.max(np.unique(trg.sum(axis=0)))
+    wstart = np.argmax(trg.sum(axis=1))
+    wend = int(wstart+w)
+    return (wstart, wend, hstart, hend)
