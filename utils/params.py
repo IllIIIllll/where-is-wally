6e61961849a647d6c0ff70523ec703c0701d5e0e
JISUNG PARK <llllllllll@kakao.com>
2021-01-03 18:13:40 +0900

    feat: Add parameter module
    - XML_PATH
    - TRG_PATH
    - IMG_PATH
    - mu
    - std

---
+++ utils/params.py
@@ -0,0 +1,9 @@
+# © 2021 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# MIT License
+
+XML_PATH = 'imgs/bnd_box/'
+TRG_PATH = 'imgs/target_imgs/'
+IMG_PATH = 'imgs/original_imgs/'
+mu = 0.57417726724528806
+std = 0.31263486676782137
